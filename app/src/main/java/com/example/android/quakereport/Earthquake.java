aa65b767e2670f9b89cc70d72868d12321c92e34
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-06-15 15:56:56 +0100

    implement url within earthquake object

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -5,16 +5,19 @@ public class Earthquake {
     private double mMagnitude;
     private String mLocation;
     private long mTimeMilliseconds;
+    private String mUrl;
 
     /**
      * @param magnitude is the size of the earthquake
      * @param location is the city of the earthquake
-     * @param mTimeMilliseconds is the unix time in millsieconds when the earthquake occurred
+     * @param timeMilliseconds is the unix time in millsieconds when the earthquake occurred
+     * @param url is the url of the earthquake on USGS website
      */
-    public Earthquake(Double magnitude, String location, long mTimeMilliseconds) {
+    public Earthquake(Double magnitude, String location, long timeMilliseconds, String url) {
         this.mMagnitude = magnitude;
         this.mLocation = location;
-        this.mTimeMilliseconds = mTimeMilliseconds;
+        this.mTimeMilliseconds = timeMilliseconds;
+        this.mUrl = url;
     }
 
     public double getMagnitude() {
@@ -27,5 +30,9 @@ public class Earthquake {
 
     public long getTimeMilliseconds() {
         return mTimeMilliseconds;
+    }
+
+    public String getUrl() {
+        return mUrl;
     }
 }
